53720d91c8c884608b90f53f99b88fac1814d4c3
Joska Potin <jpotin@gmail.com>
2021-08-01 20:31:33 +0200

    better comments

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,35 +30,30 @@ function closeModal() {
 closeModalBtn.addEventListener('click', closeModal);
 
 // set some variables
+// we need the form and the submit button of that form
 const form = document.forms.reserve;
-const prenom = form.first;
-const nom = form.last;
-const { email } = form;
-const { birthdate } = form;
-const { quantity } = form;
-const locations = form.location;
-const cgu = form.checkbox1;
 const submitBtn = form.submit;
 
-// Display or hide error messages
-// Select the parent and set the attribut "data-error-visible"
+// display or hide error messages function
+// Select the parent and set a value to true or false to the attribut "data-error-visible"
+// The CSS handle the display
 const errorVisible = (e, value) => {
   e.parentNode.dataset.errorVisible = value;
 };
 
-// email validation
+// email validation function
 // regular expression found here http://emailregex.com/
 const mailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 // if email match regex return true else return false
 const validateEmail = (item) => item.value.match(mailformat);
 
-// location validation
+// location validation function
 const validateLocation = (items) => {
   // will be set to true if there is a location checked
   let locationIsValid = false;
 
   // loop through the items
-  // if one is checked return true
+  // if one is checked set locationIsValid to true
   items.forEach((item) => {
     if (item.checked) {
       locationIsValid = true;
@@ -69,27 +64,37 @@ const validateLocation = (items) => {
   return locationIsValid;
 };
 
-// validation
+// form validation function
 const validate = () => {
+  // store the inputs that need validation
+  const prenom = form.first;
+  const nom = form.last;
+  const { email } = form;
+  const { birthdate } = form;
+  const { quantity } = form;
+  const locations = form.location;
+  const cgu = form.checkbox1;
+
   // will be set to false if there is any invalid input
+  // will be read at the end
   let formIsValid = true;
 
   // let's use html5 validation API for those inputs
   const inputs = [prenom, nom, birthdate, quantity, cgu];
-
   inputs.forEach((input) => {
     // if is not valide set data-error-visible to true and set formIsValid to false
     // else set data-error-visible to false
-    if (input.checkValidity() === false) {
+    if (input.checkValidity()) {
+      errorVisible(input, 'false');
+    } else {
       errorVisible(input, 'true');
       formIsValid = false;
-    } else {
-      errorVisible(input, 'false');
     }
   });
 
   // check if the email is valid
-  // if not set formIsValid to false
+  // if is not valide set data-error-visible to true and set formIsValid to false
+  // else set data-error-visible to false
   if (validateEmail(email)) {
     errorVisible(email, 'false');
   } else {
@@ -97,8 +102,9 @@ const validate = () => {
     formIsValid = false;
   }
 
-  // check if alocation is selected
-  // if not set formIsValid to false
+  // check if a location is selected
+  // if is not valide set data-error-visible to true and set formIsValid to false
+  // else set data-error-visible to false
   if (validateLocation(locations)) {
     errorVisible(locations[0], 'false');
   } else {
