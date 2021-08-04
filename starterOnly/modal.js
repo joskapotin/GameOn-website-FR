60523b467291348a984d7d1763255e64f3e23c43
Joska Potin <jpotin@gmail.com>
2021-08-04 14:49:42 +0200

    rename and separate function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,12 @@ const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const hamburger = document.querySelector('.hamburger');
 const closeModalBtn = document.querySelector('.close-modal');
+// select the form
+const uiFormReserve = document.forms.reserve;
+// select the confirmation message
+const uiConfirmationMessage = document.querySelector('.confirmation-message');
+// select the close-form button
+const uiCloseFormBtn = document.querySelector('.close-form');
 
 // Responsive menu
 hamburger.addEventListener('click', editNav);
@@ -36,33 +42,43 @@ closeModalBtn.addEventListener('click', closeModal);
 FORM VALIDATION SECTION
 */
 
-// select the form
-const form = document.forms.reserve;
-
-// we also need a variable that will be our form validation token
+// we need a variable that will be our form validation token
 // if any input is not valid we update this variable to false
 let formIsValid = true;
 
 // display or hide error messages function
 // Select the parent and set a value to true or false to the attribut "data-error-visible"
 // The CSS handle the display
-const toggleErrorMsg = (element, value) => {
-  const el = element;
-  el.parentNode.dataset.errorVisible = value;
+const showError = (uiElement, value) => {
+  const uiEl = uiElement;
+  uiEl.parentNode.dataset.errorVisible = value;
+};
+
+// toogle show/hide function
+const toggleVisible = (uiElement) => {
+  const uiEl = uiElement.classList;
+
+  if (uiEl.contains('hide')) {
+    uiEl.remove('hide');
+    uiEl.add('show');
+  } else {
+    uiEl.remove('show');
+    uiEl.add('hide');
+  }
 };
 
 // html5 validation API function
-const validateHtml = (elements) => {
-  elements.forEach((element) => {
+const validateHtml = (uiElements) => {
+  uiElements.forEach((uiElement) => {
     // if an element is valide set data-error-visible to false
-    if (element.checkValidity()) {
-      return toggleErrorMsg(element, 'false');
+    if (uiElement.checkValidity()) {
+      return showError(uiElement, 'false');
     }
 
     // set the form validation token (formIsValid) to false
     formIsValid = false;
     // and set data-error-visible to true
-    return toggleErrorMsg(element, 'true');
+    return showError(uiElement, 'true');
   });
 };
 
@@ -70,55 +86,60 @@ const validateHtml = (elements) => {
 // regular expression found here http://emailregex.com/
 const mailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 // if email match regex set data-error-visible to false
-const validateEmail = (element) => {
-  if (mailformat.test(element.value)) {
-    return toggleErrorMsg(element, 'false');
+const validateEmail = (uiElement) => {
+  if (mailformat.test(uiElement.value)) {
+    return showError(uiElement, 'false');
   }
 
   // set the form validation token (formIsValid) to false
   formIsValid = false;
   // and set data-error-visible to true
-  return toggleErrorMsg(element, 'true');
+  return showError(uiElement, 'true');
 };
 
 // location validation function
-const validateLocation = (elements) => {
+const validateLocation = (uiElements) => {
   // set a validation token for this function and default false
   let locationIsValid = false;
   // loop through all the elements and if one is checked set our token (locationIsValid) to true
-  elements.forEach((element) => {
-    if (element.checked) {
+  uiElements.forEach((uiElement) => {
+    if (uiElement.checked) {
       locationIsValid = true;
     }
   });
 
   // if our token is true set data-error-visible to false
   if (locationIsValid === true) {
-    return toggleErrorMsg(elements[0], 'false');
+    return showError(uiElements[0], 'false');
   }
 
   // set the form validation token (formIsValid) to false
   formIsValid = false;
   // and set data-error-visible to true
-  return toggleErrorMsg(elements[0], 'true');
+  return showError(uiElements[0], 'true');
 };
 
-// submit form function
-const submitForm = () => {
-  console.log('form is valid');
-  form.submit();
+// reset function
+const resetForm = () => {
+  uiFormReserve.reset();
+  toggleVisible(uiFormReserve);
+  toggleVisible(uiConfirmationMessage);
 };
 
-// confirmation message function
-const confirmSubmit = () => {
-  // hide form and show confirmation message
-  form.style.opacity = 0;
-  const confirmationMessage = document.querySelector('.confirmation-message');
-  confirmationMessage.classList.add('show-message');
+// exit function
+const exitForm = () => {
+  // on click reset and close modal
+  uiCloseFormBtn.addEventListener('click', () => {
+    resetForm();
+    closeModal();
+  });
+};
 
-  // on click execute submitForm function
-  const confirmSubmitBtn = document.querySelector('.confirm-submit');
-  confirmSubmitBtn.addEventListener('click', submitForm);
+// hide the form and show the confirmation message
+const showConfirmationMessage = () => {
+  toggleVisible(uiFormReserve);
+  toggleVisible(uiConfirmationMessage);
+  exitForm();
 };
 
 // form validation function
@@ -129,7 +150,7 @@ const validate = () => {
   // store the inputs that we need to validate in const
   const {
     first, last, email, birthdate, quantity, location, checkbox1,
-  } = form;
+  } = uiFormReserve;
 
   // let's use html5 validation API for those inputs
   const inputs = [first, last, birthdate, quantity, checkbox1];
@@ -143,17 +164,18 @@ const validate = () => {
 
   // if formIsValid is false stop the function
   if (formIsValid === false) {
-    console.log('form not valid');
     return false;
   }
 
   // if we got here it means that ou form is valid so we can submit
-  return confirmSubmit();
+  // Pretend that we have submit it and received a confirmation
+  // we can show a confirmation message
+  return showConfirmationMessage();
 };
 
 // prevent the browser from showing default error
 // the html5 validation triggers on click not on submit, so we need to prevent the default event
-form.btnSubmit.addEventListener('click', (e) => {
+uiFormReserve.btnSubmit.addEventListener('click', (e) => {
   e.preventDefault();
   validate();
 });
